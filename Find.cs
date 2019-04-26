dba58ff2e3193c6b8cea06c3f0a863356e77a430
Raziel <razzziel001@gmail.com>
2019-04-26 12:27:25 -0500

    Inclui script que detecta con la función awake la ubicación de un objeto

---
+++ Find.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Find : MonoBehaviour {
+    void Awake()
+    {
+        GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        Debug.Log(camera.transform.position);
+    }
+	
+}
